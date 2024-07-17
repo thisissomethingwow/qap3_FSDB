30f341b0de20395ff9188776760b0c46b376b865
Evan <evan.pittman@keyin.com>
2024-07-17 19:33:33 -0230

    added del put patch for book

--- route/books.js
+++ route/books.js
@@ -12,4 +12,62 @@ router.get('/',async(req,res)=>{
     }
 })
 
+router.post('/',async(req,res)=>{
+    if(DEBUG) console.log("we be posting in books");
+    try {
+        await booksDal.addBooks(req.body.title, req.body.desc)
+        if(DEBUG) console.log("done add in books");
+        res.redirect('/books/')
+    } catch {
+        res.render('503')
+    }
+})
+
+router.get('/:id/replace',async (req,res)=>{
+    if(DEBUG)console.log("we be replaceing in books")
+    res.render('putBook.ejs',{title: req.query.title, desc: req.query.desc, theId: req.params.id})
+})
+
+router.get('/:id/edit',async (req,res)=>{
+    if(DEBUG)console.log("we be editing in books")
+    res.render('patchBook.ejs',{title: req.query.title, desc: req.query.desc, theId: req.params.id})
+})
+
+router.get('/:id/delete',async (req,res)=>{
+    if(DEBUG)console.log("we be deleting in books")
+    res.render('deleteBook.ejs',{title: req.query.title, desc: req.query.desc, theId: req.params.id})
+})
+
+router.put('/:id',async(req,res)=>{
+    if(DEBUG)console.log("this is put in books");
+    try {
+        await booksDal.putBooks(req.params.id,req.body.title, req.body.desc)
+        res.redirect('/books/')
+    } catch{
+        res.render('503')
+    }
+})
+
+
+router.patch('/:id',async(req,res)=>{
+    if(DEBUG)console.log("this is patch in books");
+    try {
+        await booksDal.patchBooks(req.params.id,req.body.title, req.body.desc)
+        res.redirect('/books/')
+    } catch{
+        res.render('503')
+    }
+})
+
+router.delete('/:id',async(req,res)=>{
+    if(DEBUG)console.log("this is del in books");
+    try {
+        await booksDal.delBooks(req.params.id)
+        res.redirect('/books/')
+    } catch{
+        res.render('503')
+    }
+})
+
+
 module.exports = router
--- services/books.dal.js
+++ services/books.dal.js
@@ -1,5 +1,6 @@
 const dal = require('./auth');
-let getBooks = ()=>{
+
+const getBooks = ()=>{
     if(DEBUG) console.log("books.dal.js")
     return new Promise(function(resolve,reject){
         const sql = "SELECT book_id AS _id, book_title AS title, book_description AS desc FROM books \
@@ -14,6 +15,72 @@ dal.query(sql,[],(err,result)=>{
 })
 }) 
 }
+
+const addBooks = (title,desc)=>{
+    if(DEBUG) console.log("we add books")
+    return new Promise(function(resolve,reject){
+        const sql = "INSERT INTO public.books(book_title, book_description) \
+        VALUES ($1, $2);";
+dal.query(sql,[title,desc],(err,result)=>{
+    if(err){
+        if(DEBUG)console.log(err)
+        reject(err)
+    }else{
+        resolve(result.rows)
+        return
+    }
+    })
+  }) 
+}
+
+
+const putBooks = (id,title,desc)=>{
+    if(DEBUG)console.log("we be putting with books")
+    return new Promise((resolve,reject)=>{
+        const sql = "UPDATE public.books SET book_title=$2, book_description=$3 WHERE book_id=$1;";
+        dal.query(sql,[id,title,desc],(err,result)=>{
+            if(err){
+                reject(err)
+            }else{
+                resolve(result.rows)
+            }
+    })
+})
+}
+
+const patchBooks = (id,title,desc)=>{
+    if(DEBUG)console.log("we be patching in books")
+    return new Promise((resolve,reject)=>{
+        const sql = "UPDATE public.books SET book_title=$2, book_description=$3 WHERE book_id=$1;";
+        dal.query(sql,[id,title,desc],(err,result)=>{
+            if(err){
+                reject(err)
+            }else{
+                resolve(result.rows)
+            }
+    })
+})
+}
+
+const delBooks = (id)=>{
+    if(DEBUG)console.log("we be deling")
+    return new Promise((resolve,reject)=>{
+        const sql = "DELETE FROM public.books WHERE book_id = $1;";
+        dal.query(sql,[id],(err,result)=>{
+            if(err){
+                reject(err)
+            }else{
+                resolve(result.rows)
+            }
+    })
+})
+}
+
+
 module.exports={
     getBooks,
+    addBooks,
+    putBooks,
+    patchBooks,
+    delBooks,
 }
