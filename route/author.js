0c95e018e1fff39aa85f6a49aa0b2a3c2eb6f88d
Evan <evan.pittman@keyin.com>
2024-07-17 18:43:17 -0230

    got del to work for books

--- route/author.js
+++ route/author.js
@@ -12,6 +12,17 @@ router.get('/',async(req,res)=>{
     }
 })
 
+// router.get('/:id',async (req,res)=>{
+//     try {
+//         const anAuthor = await authorsDal.getAuthorsById(req.params.id
+        
+//         )
+//     } catch{
+//         res.render("503")
+//     }
+// })
+
+
 router.post('/',async(req,res)=>{
     if(DEBUG) console.log("we be posting");
     try {
@@ -23,5 +34,52 @@ router.post('/',async(req,res)=>{
     }
 })
 
+router.get('/:id/replace',async (req,res)=>{
+    if(DEBUG)console.log("we be replaceing")
+    res.render('put.ejs',{fn: req.query.fn, ln: req.query.ln, theId: req.params.id})
+})
+
+router.get('/:id/edit',async (req,res)=>{
+    if(DEBUG)console.log("we be editing")
+    res.render('patch.ejs',{fn: req.query.fn, ln: req.query.ln, theId: req.params.id})
+})
+
+router.get('/:id/delete',async (req,res)=>{
+    if(DEBUG)console.log("we be deleting")
+    res.render('delete.ejs',{fn: req.query.fn, ln: req.query.ln, theId: req.params.id})
+})
+
+
+
+router.put('/:id',async(req,res)=>{
+    if(DEBUG)console.log("this is put");
+    try {
+        await authorsDal.putAuthors(req.params.id,req.body.fn, req.body.ln)
+        res.redirect('/authors/')
+    } catch{
+        res.render('503')
+    }
+})
+
+
+router.patch('/:id',async(req,res)=>{
+    if(DEBUG)console.log("this is patch");
+    try {
+        await authorsDal.patchAuthors(req.params.id,req.body.fn, req.body.ln)
+        res.redirect('/authors/')
+    } catch{
+        res.render('503')
+    }
+})
+
+router.delete('/:id',async(req,res)=>{
+    if(DEBUG)console.log("this is del");
+    try {
+        await authorsDal.delAuthors(req.params.id)
+        res.redirect('/authors/')
+    } catch{
+        res.render('503')
+    }
+})
 
 module.exports = router
