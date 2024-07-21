e29afa4812620bff8c2973e6d16ce5f3bcc4539e
Evan <evan.pittman@keyin.com>
2024-07-21 13:49:16 -0230

    made everything look better

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.set('view engine','ejs')
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true, })); 
 app.use(methodOverride('_method')); 
-app.use(express.static(__dirname+'/public'))
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.render('index.ejs');
